590fb98851257ecac24e7bfec2a7b891f479cd63
Dragonclaw101 <Huppjoesaphean@gmail.com>
2021-02-23 08:59:25 -0800

    Started object practice.'

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+console.log('Hello World!');
+
+//This is an object literal - I define every part of the object.
+let car = {
+    make: 'Ford',
+    model: 'F1-50',
+    passengers: 5,
+
+}
+
+//print make & model of the car
+
+console.log('My car is a ' + car.make + ' ' + car.model);
+console.log('The ' + car.make + ' ' + car.model + ' carries ' + car.passengers + ' passengers');
